aa5e6a4470d308950bf2e32e8a4a6767dd26d7b6
hanawazaki <hanawazaki002@gmail.com>
2020-09-21 21:45:45 +0700

    first

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2>
+            <strong>Delicious Food Menu,</strong>
+            <br />in Your Gadget
+          </h2>
+          <p>Ayo segera pilih dan pesan makanan favorit Anda</p>
+          <button class="btn btn-lg btn-success">Pesan</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img src="../assets/images/hero.png" alt width="100%" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Hero",
+};
+</script>
